c5658e422fb920353d2047ab6564f7af96fc3203
bimon-alta <bimon@alterra.id>
2019-12-21 20:07:18 +0700

    03 revisi tanpa Class, yang lebih sederhana

--- scripts/image_filtering_resource.py
+++ scripts/image_filtering_resource.py
@@ -1,14 +1,3 @@
-from flask import Blueprint
-from flask_restful import Api, reqparse, Resource, marshal, inputs
-from sqlalchemy import desc
-import datetime
-
-from blueprints import app
-
-from blueprints import internal_required
-from flask_jwt_extended import jwt_required
-
-
 import hmac,hashlib
 import requests
 
@@ -16,48 +5,30 @@ import xmltodict, json
 import time                 #mengambil fungsi delay
 
 
-bp_filter = Blueprint('the_filter', __name__)  
-api = Api(bp_filter)
 
-class FilterResource(Resource):
-    img_task_api_host = 'http://opeapi.ws.pho.to/addtask/'
-    img_get_api_host = 'http://opeapi.ws.pho.to/getresult'
+img_task_api_host = 'http://opeapi.ws.pho.to/addtask/'
+img_get_api_host = 'http://opeapi.ws.pho.to/getresult'
 
-    filter_blur = 'blur'
-    filter2 = 'color_dominance'
+filter_blur = 'blur'
+filter2 = 'color_dominance'
 
-######################### method POST INI HANYA UTK TESTING VIA POSTMAN ###########################
-#########################       JANGAN PANGGIL ATAU COMMENT SAJA        ###########################
-    def __init__(self):
-        pass
+
+
+def main(p_app_id, p_app_key, p_img_url):
+
+    result = proceedBlurring(p_app_id,  p_app_key, p_img_url)
     
-    # @jwt_required
-    # @internal_required
-    def post(self):
-        # self.the_app_id = args['app_id']
-        # self.the_key = args['key']
+    if result['status']=='successful':
+        time.sleep(3)
+        result1 = proceedDesaturate(p_app_id, p_app_key, p_img_url)
+        print(result1)
+        if result1['status']=='successful':
 
-        parser = reqparse.RequestParser()
-        parser.add_argument('app_id', location='json', required=True)
-        parser.add_argument('key',  location='json', required=True)
-        parser.add_argument('img_url', location='json', required=True)
-
-
-        args = parser.parse_args()
-
-    
-        result = self.proceedBlurring(args['app_id'], args['key'], args['img_url'])
-        if result['status']=='successful':
-            time.sleep(3)
-            result1 = self.proceedDesaturate(args['app_id'], args['key'], result['img_url'])
-            print(result1)
-            if result1['status']=='successful':
-
-                return {'status':'successful', 'img_url':result1['img_url']}
-            else:
-                return {'status':result1['status'], 'message':result1['message']}
+            return {'status':'successful', 'img_url':result1['img_url']}
         else:
-            return {'status':result['status'], 'message':result['message']}
+            return {'status':result1['status'], 'message':result1['message']}
+    else:
+        return {'status':result['status'], 'message':result['message']}
 
 #########################################################################################
 
@@ -65,18 +36,48 @@ class FilterResource(Resource):
 
 
 
-    def proceedDesaturate(self, app_id, app_key, img_url):
+def proceedDesaturate(app_id, app_key, img_url):
 
-        #MENDEKATI HITAM PUTIH
-        # the_data = '<image_process_call><image_url>'+img_url+'</image_url><methods_list><method><name>'+ self.filter2 +'</name><params>hue=3.5;fixed_saturation=0.3</params></method></methods_list></image_process_call>'
+    #MENDEKATI HITAM PUTIH
+    # the_data = '<image_process_call><image_url>'+img_url+'</image_url><methods_list><method><name>'+ filter2 +'</name><params>hue=3.5;fixed_saturation=0.3</params></method></methods_list></image_process_call>'
+    
+    #COVER COLOR BIRU
+    the_data = '<image_process_call><image_url>'+img_url+'</image_url><methods_list><method><name>'+ filter2 +'</name><params>hue=3.5;fixed_saturation=0.6</params></method></methods_list></image_process_call>'
+    
+    the_sign_data = hmac.new(bytes(app_key, 'utf8'), bytes(the_data, 'utf8'), hashlib.sha1).hexdigest()
+    form_data = {'app_id': app_id, 'sign_data': the_sign_data, 'data': the_data}
+
+    rq = requests.post(img_task_api_host, data=form_data)
+
+    resp0 = json.dumps(xmltodict.parse(rq.text))
+
+    resp = json.loads(resp0)
+
+    if (resp is not None) and (resp['image_process_response']['status'].lower() == 'ok'):
+        req_id = resp['image_process_response']['request_id']
+
+        result = proceedReqId(req_id)
         
-        #COVER COLOR BIRU
-        the_data = '<image_process_call><image_url>'+img_url+'</image_url><methods_list><method><name>'+ self.filter2 +'</name><params>hue=3.5;fixed_saturation=0.6</params></method></methods_list></image_process_call>'
+        if (result != None) and (result['status']=='successful'):
+            return {'status':'successful', 'img_url':result['img_url']}
+        else:
+            return {'status':'failed', 'message':'third party request error, Failed proceed get req id'}
+    else:
+        return {'status':'failed', 'message':'third party request error, Failed proceed task'}
+
+
+def proceedBlurring(app_id, app_key, img_url, count_no=3):
+
+    if count_no < 1:
+        return {'status':'successful','img_url':img_url}
+
+    else:
+        the_data = '<image_process_call><image_url>'+img_url+'</image_url><methods_list><method><name>'+ filter_blur +'</name><params>radius</params></method></methods_list></image_process_call>'
         
         the_sign_data = hmac.new(bytes(app_key, 'utf8'), bytes(the_data, 'utf8'), hashlib.sha1).hexdigest()
         form_data = {'app_id': app_id, 'sign_data': the_sign_data, 'data': the_data}
 
-        rq = requests.post(self.img_task_api_host, data=form_data)
+        rq = requests.post(img_task_api_host, data=form_data)
 
         resp0 = json.dumps(xmltodict.parse(rq.text))
 
@@ -85,69 +86,35 @@ class FilterResource(Resource):
         if (resp is not None) and (resp['image_process_response']['status'].lower() == 'ok'):
             req_id = resp['image_process_response']['request_id']
 
-            result = self.proceedReqId(req_id)
-            
+            result = proceedReqId(req_id)
+
             if (result != None) and (result['status']=='successful'):
-                return {'status':'successful', 'img_url':result['img_url']}
+                time.sleep(3)
+                return proceedBlurring(app_id, app_key, result['img_url'], count_no-1)
             else:
                 return {'status':'failed', 'message':'third party request error, Failed proceed get req id'}
         else:
-            return {'status':'failed', 'message':'third party request error, Failed proceed task'}
+            return {'status':'failed', 'message':'third party request error, Failed proceed task'}   
+        
 
-
-    def proceedBlurring(self, app_id, app_key, img_url, count_no=3):
-    
-        if count_no < 1:
-            return {'status':'successful','img_url':img_url}
-
-        else:
-            the_data = '<image_process_call><image_url>'+img_url+'</image_url><methods_list><method><name>'+ self.filter_blur +'</name><params>radius</params></method></methods_list></image_process_call>'
+def proceedReqId(request_id):
+    rq = requests.get(img_get_api_host, params={'request_id':request_id})
             
-            the_sign_data = hmac.new(bytes(app_key, 'utf8'), bytes(the_data, 'utf8'), hashlib.sha1).hexdigest()
-            form_data = {'app_id': app_id, 'sign_data': the_sign_data, 'data': the_data}
-
-            rq = requests.post(self.img_task_api_host, data=form_data)
-
-            resp0 = json.dumps(xmltodict.parse(rq.text))
-
-            resp = json.loads(resp0)
-
-            if (resp is not None) and (resp['image_process_response']['status'].lower() == 'ok'):
-                req_id = resp['image_process_response']['request_id']
-
-                result = self.proceedReqId(req_id)
-
-                if (result != None) and (result['status']=='successful'):
-                    time.sleep(3)
-                    return self.proceedBlurring(app_id, app_key, result['img_url'], count_no-1)
-                else:
-                    return {'status':'failed', 'message':'third party request error, Failed proceed get req id'}
-            else:
-                return {'status':'failed', 'message':'third party request error, Failed proceed task'}   
-            
-    
-    def proceedReqId(self, request_id):
-        rq = requests.get(self.img_get_api_host, params={'request_id':request_id})
-                
-        if rq.status_code != 200:
-            return {'status':'failed', 'message':'error dari third party api'}, 400
+    if rq.status_code != 200:
+        return {'status':'failed', 'message':'error dari third party api'}, 400
 
 
-        resp0 = json.dumps(xmltodict.parse(rq.text))
+    resp0 = json.dumps(xmltodict.parse(rq.text))
 
-        resp = json.loads(resp0)
+    resp = json.loads(resp0)
 
-        if resp is not None:
-            if resp['image_process_response']['status'].lower() == 'ok':
-                img_url = resp['image_process_response']['nowm_image_url']
+    if resp is not None:
+        if resp['image_process_response']['status'].lower() == 'ok':
+            img_url = resp['image_process_response']['nowm_image_url']
 
-                return {'status':'successful', 'img_url':img_url}
-        else:
-            return {'status':'failed', 'message':'error dari third party api'}, 400
+            return {'status':'successful', 'img_url':img_url}
+    else:
+        return {'status':'failed', 'message':'error dari third party api'}, 400
 
 
-
-######################### BARIS CODE INI HANYA UTK ROUTING URL REQUEST  ###########################
-#########################       JANGAN PANGGIL ATAU COMMENT SAJA        ###########################
-api.add_resource(FilterResource,'')   
 
